e73eabe69808d8553c687276a1ec95ec493f78c4
Hodoniczki Armand <hodoarmanddev@gmail.com>
2024-10-22 23:34:57 +0200

    Added colored logs for better readability.

--- src/run_cli.py
+++ src/run_cli.py
@@ -4,6 +4,8 @@ import os.path
 from argparse import Namespace, ArgumentParser
 from pathlib import WindowsPath, PosixPath
 
+import coloredlogs
+
 from playlist_to_album_exporter import PlaylistToAlbumExporter
 from playlist_exporter_configuration import PlaylistExporterConfiguration
 from utility.check_python_version import check_python_version
@@ -11,7 +13,8 @@ from utility.check_python_version import check_python_version
 def run_cli() -> int:
     """ Run the CLI API for the application """
 
-    logging.basicConfig(level=logging.DEBUG)
+    # coloredlogs.install(level='DEBUG', fmt='%(levelname)s: %(message)s')
+    coloredlogs.install(level='DEBUG', fmt='%(levelname)s|%(name)s: %(message)s')
     logger = logging.getLogger("Playlist Exporter CLI Utility")
 
     check_python_version()
@@ -34,7 +37,8 @@ def run_cli() -> int:
     logger.info("Configuration: %s", exporter_config)
 
     exporter = PlaylistToAlbumExporter(exporter_config)
-    exporter.export_album()
+    if not exporter.export_album():
+        return 1
 
     return 0
 
